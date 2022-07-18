0d86e17473d848f118065b910bfaa819c3f6d8f7
freejin23 <specialeo1@likelion.org>
2022-07-18 09:16:58 +0900

    [LIKELION-0010] Main page Update

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ st.sidebar.markdown("# Stanford Dogs Data Info🐶")
 st.markdown('# CV Mini Project')
 st.markdown("## Stanford Dogs Dataset (kaggle) 🐶")
 st.markdown(
-    '#### [Stanford Dogs Dataset](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
+    '#### [Stanford Dogs Dataset link](https://www.kaggle.com/datasets/jessicali9530/stanford-dogs-dataset?datasetId=119698&sortBy=voteCount)')
 st.markdown("강아지 사진을 올리면 무슨 종에 가까운지 알려줍니다!")
 
 st.markdown("![Alt Text](https://media.giphy.com/media/kxlDaFDVzTLb2/giphy.gif)")
 
 st.markdown('---')
 
-st.markdown("#### 팀장: 주소리")
-st.markdown("#### 팀원: 이주승, 김재석")
+st.markdown('#### 팀장: 주소리')
+st.markdown('#### 팀원: 이주승, 김재석')
